85d263a1e3d7497f43b0eee629579d2409bc33cd
coxfactor <117258048+coxfactor@users.noreply.github.com>
2022-12-07 16:18:18 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 </head>
 <body>
     <h1>Ex. 사용자가 원하는 구구단 출력하기</h1>
-    <form method="POST" action="inputGugudan.php">
+    <form method="POST" action="index.php">
         <li>구구단을 보고 싶은 숫자를 입력하고 제출을 누르세요.</li>
         <input type="text" name="input"/><br/>
         <input type="submit" name="answer">
     </form>
+
 <?php
 if (isset($_POST['input'])){
     $num = (int)($_POST['input']);
