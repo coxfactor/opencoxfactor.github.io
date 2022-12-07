ab958a796e23e9ad4e6769660d86dbac56e160af
coxfactor <117258048+coxfactor@users.noreply.github.com>
2022-12-07 17:59:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,10 @@
     <title></title>
 </head>
 <body>
-    <h1>Ex. 사용자가 원하는 구구단 출력하기</h1>
-    <form method="POST" action="index.php">
-        <li>구구단을 보고 싶은 숫자를 입력하고 제출을 누르세요.</li>
-        <input type="text" name="input"/><br/>
-        <input type="submit" name="answer">
-    </form>
-
-<?php
-if (isset($_POST['input'])){
-    $num = (int)($_POST['input']);
-    echo $num."단 입니다.<br/>";
-    for($j=1;$j<=9;$j++){
-        echo $num." X ".$j." = ".($num*$j)."<br/>";
-    }
-    echo "<br/>";
-}
- 
-?>
+    <h1>문제2. 이미지 출력하기</h1>
+    <?php
+        echo "아래에 이미지가 출력됩니다.<br/>";
+        echo "<img src='costco.png'/>";
+    ?>
 </body>
 </html>
